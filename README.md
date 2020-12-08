6245726e59200d6dd801f879e5c6231e484fd486
Chad Capra <chadcapra@gmail.com>
2020-12-08 13:30:38 -0600

    add development script for resplice

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 This project is the umbrella project for running Resplice locally.
 
+To set up resplice application for development, just run the following script
+(note: bash shell required)
 
+`sh <(curl -L https://raw.githubusercontent.com/ChadCapra/resplice/main/bin/setup.sh)`
---
+++ bin/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Clone replice (umbrella) repo
+git clone git@github.com:ChadCapra/resplice.git resplice
+
+# Change into resplice folder
+cd resplice
+
+# Clone replice api
+git clone git@github.com:ChadCapra/api.resplice.com api.resplice.com
+
+# Clone replice pwa
+git clone git@github.com:ChadCapra/pwa.resplice.com pwa.resplice.com
