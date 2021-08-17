a65d27c0ee8c8252721c008fb083715f50d9bf5c
Chad Capra <chadcapra@gmail.com>
2021-08-17 17:02:46 -0500

    add proto

--- setup.sh
+++ setup.sh
@@ -9,5 +9,8 @@ cd resplice
 # Clone replice api
 git clone git@github.com:ChadCapra/api.resplice.com api
 
+# Clone replice proto
+git clone git@github.com:ChadCapra/resplice.proto proto
+
 # Clone replice www
 git clone git@github.com:ChadCapra/www.resplice.com www
